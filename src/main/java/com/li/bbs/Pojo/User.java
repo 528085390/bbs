3b778758e9e4579443ec255b1ec8cdd4214aa06d
li <528085390@qq.com>
2025-09-12 00:00:33 +0800

    test
    Signed-off-by: li <528085390@qq.com>

---
+++ src/main/java/com/li/bbs/Pojo/User.java
@@ -0,0 +1,22 @@
+package com.li.bbs.Pojo;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+import java.time.LocalDateTime;
+
+@EntityScan
+@Data
+@AllArgsConstructor
+public class User {
+    Integer id;
+    String username;
+    String password;
+    String email;
+    String avatarUrl;
+    LocalDateTime createTime;
+    LocalDateTime updateTime;
+    Integer status;
+}
