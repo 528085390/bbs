8987ee8d12a5313e12dc1ff74c99866cf18db705
周卓熙 <123>
2025-10-03 19:04:39 +0800

    增加了登录注册界面

--- src/main/web/js/index.js
+++ src/main/web/js/index.js
@@ -1,3 +1,4 @@
+// 整体三条的画面
 // 1. 获取DOM元素
 const leftNav = document.querySelector('.content .left_nav');
 const rightTopicArea = document.querySelector('.content .right_topic_area');
@@ -14,3 +15,38 @@ function calculateMiddleWidth() {
 // 3. 绑定事件，确保响应式
 window.addEventListener('load', calculateMiddleWidth);
 window.addEventListener('resize', calculateMiddleWidth);
+
+
+
+
+
+
+
+
+
+
+// 注册登录界面
+const screen = document.querySelector('#screen');
+const container = document.querySelector('#container');
+const signInButton = document.querySelector('#signIn');
+const signUpButton = document.querySelector('#signUp');
+const cancel = document.querySelector('.cancel');
+const login = document.querySelector('#login');
+
+
+signUpButton.addEventListener('click', () => container.classList.add('right-paner-active'));
+signInButton.addEventListener('click', () => container.classList.remove('right-paner-active'));
+
+
+cancel.addEventListener('click', () => {
+    container.style.display = "none";
+    cancel.style.display = "none";
+    screen.style.display = "none";
+
+})
+
+login.addEventListener('click', () => {
+    container.style.display = "block";
+    cancel.style.display = "block";
+    screen.style.display = "block";
+})
