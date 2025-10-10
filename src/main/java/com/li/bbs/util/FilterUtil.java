1ac12f323ee558eeca6994c99853fb84c781800e
li <528085390@qq.com>
2025-10-10 14:17:00 +0800

    修改注解
    Signed-off-by: li <528085390@qq.com>

--- src/main/java/com/li/bbs/util/FilterUtil.java
+++ src/main/java/com/li/bbs/util/FilterUtil.java
@@ -1,50 +1,50 @@
-package com.li.bbs.util;
-
-import jakarta.servlet.*;
-import jakarta.servlet.annotation.WebFilter;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
-import java.io.IOException;
-
-
-@Component
-@WebFilter(urlPatterns = "/*")
-public class FilterUtil implements Filter {
-
-    @Autowired
-    private JwtUtil jwtUtil;
-
-    @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-
-    }
-
-    @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        HttpServletRequest Request = (HttpServletRequest) servletRequest;
-        HttpServletResponse Response = (HttpServletResponse) servletResponse;
-        String uri = Request.getRequestURI();
-        if(uri.contains("/auth")){
-            filterChain.doFilter(servletRequest,servletResponse);
-            return;
-        }
-        String token = Request.getHeader("token");
-        if(token == null || token.isEmpty()){
-            Response.setStatus(401);
-            return;
-        }
-        if(jwtUtil.isTokenExpired(token)){
-           Response.setStatus(401);
-        }
-        filterChain.doFilter(servletRequest,servletResponse);
-
-    }
-
-    @Override
-    public void destroy() {
-
-    }
-}
+//package com.li.bbs.util;
+//
+//import jakarta.servlet.*;
+//import jakarta.servlet.annotation.WebFilter;
+//import jakarta.servlet.http.HttpServletRequest;
+//import jakarta.servlet.http.HttpServletResponse;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Component;
+//
+//import java.io.IOException;
+//
+//
+//@Component
+//@WebFilter(urlPatterns = "/*")
+//public class FilterUtil implements Filter {
+//
+//    @Autowired
+//    private JwtUtil jwtUtil;
+//
+//    @Override
+//    public void init(FilterConfig filterConfig) throws ServletException {
+//
+//    }
+//
+//    @Override
+//    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+//        HttpServletRequest Request = (HttpServletRequest) servletRequest;
+//        HttpServletResponse Response = (HttpServletResponse) servletResponse;
+//        String uri = Request.getRequestURI();
+//        if(uri.contains("/auth")){
+//            filterChain.doFilter(servletRequest,servletResponse);
+//            return;
+//        }
+//        String token = Request.getHeader("token");
+//        if(token == null || token.isEmpty()){
+//            Response.setStatus(401);
+//            return;
+//        }
+//        if(jwtUtil.isTokenExpired(token)){
+//           Response.setStatus(401);
+//        }
+//        filterChain.doFilter(servletRequest,servletResponse);
+//
+//    }
+//
+//    @Override
+//    public void destroy() {
+//
+//    }
+//}
